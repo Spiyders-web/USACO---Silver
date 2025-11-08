123e19240604d5039b811f3236c5e101755cee38
Spiyder <codekarnechale@gmail.com>
2025-11-08 15:19:09 +0530

    08/11/25

---
+++ Problem_1_My_Cow_Ate_My_Homework.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MOD = 1e9 + 7;
+#define loop(i, x, n) for (int i = x; i < n; i++)
+
+void helper()
+{
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    vector<ll> suffix_sum(n);
+    vector<ll> suffix_min(n);
+
+    loop(i, 0, n) cin >> arr[i];
+
+    suffix_sum[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix_sum[i] = suffix_sum[i + 1] + arr[i];
+    }
+    suffix_min[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        ll k = min(arr[i], suffix_min[i + 1]);
+        suffix_min[i] = k;
+    }
+
+    ll best_k = -1;
+    double best_avg = -1e5;
+
+    for (int k = 1; k < n - 1; k++)
+    {
+        double avg = -1;
+
+        ll t = (suffix_sum[k] - suffix_min[k]);
+        ll d = n - k - 1;
+        avg = t / d;
+        if (avg > best_avg)
+        {
+            best_avg = avg;
+            best_k = k;
+        }
+    }
+    cout << best_k << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    helper();
+
+    return 0;
+}
---
+++ Problem_3_Breed_Counting.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MOD = 1e9 + 7;
+#define loop(i, x, n) for (int i = x; i < n; i++)
+
+void helper()
+{
+    ll n, q;
+    cin >> n >> q;
+
+    vector<ll> arr(n);
+    loop(i, 0, n) cin >> arr[i];
+
+    vector<ll> pref_1(n + 1, 0);
+    vector<ll> pref_2(n + 1, 0);
+    vector<ll> pref_3(n + 1, 0);
+
+    for (int i = 1; i <= n; i++)
+    {
+        pref_1[i] = pref_1[i - 1];
+        pref_2[i] = pref_2[i - 1];
+        pref_3[i] = pref_3[i - 1];
+
+        if (arr[i - 1] == 1)
+            pref_1[i]++;
+        if (arr[i - 1] == 2)
+            pref_2[i]++;
+        if (arr[i - 1] == 3)
+            pref_3[i]++;
+    }
+
+    while (q--)
+    {
+        ll l, r;
+        cin >> l >> r;
+        ll x = pref_1[r] - pref_1[l - 1];
+        ll y = pref_2[r] - pref_2[l - 1];
+        ll z = pref_3[r] - pref_3[l - 1];
+
+        cout << x << " " << y << " " << z << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    helper();
+
+    return 0;
+}
